0c9c3979aeb5f8e6f5bc9c9c8568d4f86ed0f791
Udeng96 <jrj60417@naver.com>
2025-02-25 23:14:07 +0900

    feat(weekly todo 대시보드): 사용자 페이지 수정
    1. user 페이지 수정

--- front/src/component/router/user/userPage.tsx
+++ front/src/component/router/user/userPage.tsx
@@ -19,10 +19,11 @@ const UserPage = () => {
         <div className={"user_container"}>
             <div className={"user_top"}>
                 {
-                    userInfo&&
+                    userInfo &&
                     <div className={"box profile"}>
                         <div className={"info_box"}>
-                            <p className={"user"}>{userInfo.name}<span>{RANK_OPTIONS.filter((rank)=> rank.value ===userInfo?.rank)[0].label} / {userInfo.depart}</span></p>
+                            <p className={"user"}>{userInfo.name}<span>{RANK_OPTIONS.filter((rank) => rank.value === userInfo?.rank)[0].label} / {userInfo.depart}</span>
+                            </p>
                             <button className={"logout"}>로그아웃</button>
                         </div>
                         <p className={"plus"}>{userInfo.email} / {userInfo.tel}</p>
@@ -44,21 +45,54 @@ const UserPage = () => {
                                     <p className={"title"}>2월 17일 월요일</p>
                                     <button className={"add_btn"}>편집하기</button>
                                 </div>
-                                <div className={"body"}></div>
+                                <div className={"body"}>
+                                    <div className={"body_item"}>
+                                        <p className={"index_icon"}>□</p>
+                                        <p className={"time morning"}>AM</p>
+                                        <p className={"work_name"}><span>성남시 고도화사업 플랫폼폼폼폼</span></p>
+                                        <p className={"work_loca"}><span>외근(성남시청)</span></p>
+                                        <p className={"work_type"}><span>API, FRONT</span></p>
+                                        <button className={"more_btn"}/>
+                                    </div>
+                                    <div className={"body_item"}>
+                                        <p className={"index_icon"}>□</p>
+                                        <p className={"time morning"}>AM</p>
+                                        <p className={"work_name"}>성남시 고도화사업 플랫폼</p>
+                                    </div>
+                                    <div className={"body_item"}>
+                                        <p className={"index_icon"}>□</p>
+                                        <p className={"time morning"}>AM</p>
+                                        <p className={"work_name"}>성남시 고도화사업 플랫폼</p>
+                                    </div>
+                                    <div className={"body_item"}>
+                                        <p className={"index_icon"}>□</p>
+                                        <p className={"time morning"}>AM</p>
+                                        <p className={"work_name"}>성남시 고도화사업 플랫폼</p>
+                                    </div>
+                                    <div className={"body_item"}>
+                                        <p className={"index_icon"}>□</p>
+                                        <p className={"time morning"}>AM</p>
+                                        <p className={"work_name"}>성남시 고도화사업 플랫폼</p>
+                                    </div>
+                                </div>
                             </div>
                             <div className={"list_item"}>
                                 <div className={"header"}>
                                     <p className={"title"}>2월 17일 월요일</p>
                                     <button className={"add_btn"}>편집하기</button>
                                 </div>
-                                <div className={"body"}></div>
+                                <div className={"body"}>
+
+                                </div>
                             </div>
                             <div className={"list_item"}>
                                 <div className={"header"}>
                                     <p className={"title"}>2월 17일 월요일</p>
                                     <button className={"add_btn"}>편집하기</button>
                                 </div>
-                                <div className={"body"}></div>
+                                <div className={"body"}>
+
+                                </div>
                             </div>
                             <div className={"list_item"}>
                                 <div className={"header"}>
@@ -76,10 +110,43 @@ const UserPage = () => {
                             </div>
                         </div>
                     </div>
+                    <div className={"content check"}>
+                        <div className={"week"}>2월 17일 월요일</div>
+                        <div className={"time_box"}>
+                            <div className={"header am"}>
+                                AM
+                            </div>
+                            <div className={"body"}>
+                                <div className={"work_box"}>
+                                    <div className={"level_area"}>
+                                        <div className={"level_title"}>□ 성남시 고도화 사업 플랫폼</div>
+                                        <div className={"level_box"}>
+                                            <div className={"level_2"}>&nbsp;&nbsp;-&nbsp;방범 대시보드 FRONT</div>
+                                            <div className={"level_3"}>&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;CCTV 마커 조정 </div>
+                                            <div className={"level_4"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+&nbsp;이렇게 조정 </div>
+                                        </div>
+                                        <div className={"level_box"}>
+                                            <div className={"level_2"}>&nbsp;&nbsp;-&nbsp;방범 대시보드 API</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"time_box"}>
+                            <div className={"header pm"}>
+                                PM
+                            </div>
+                            <div className={"body"}>
+                                <div className={"work_box"}>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            </div>
+        </div>
 
 
     )
